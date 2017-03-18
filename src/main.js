95e40257986bc49269ae578ab399730527c674f5
scoobaroo <scoobaroo415@gmail.com>
2017-03-17 18:05:27 -0700

    created cohesion calculation to makeReport

--- src/main.js
+++ src/main.js
@@ -4,12 +4,44 @@ var makeReport = function(projectFile) {
    var classes = tools.collectElements(project, "_type", "UMLClass");
    console.log("project classes:");
    for(var i in classes) {
+      var methAttAssoc = 0
       console.log("  " + classes[i].name);
       var ops = tools.collectElements(classes[i], "_type", "UMLOperation");
-      console.log("    operations:");
+      var atts = tools.collectElements(classes[i], "_type", "UMLAttribute");
+      var numAtts = 0
+      console.log("    Attributes:");
+      for(var j in atts) {
+        console.log("      " +atts[j].name);
+        numAtts+=1
+      }
+      console.log("    Operations:");
+      var numOps=0
       for(var k in ops) {
         console.log("      " + ops[k].name);
+        numOps+=1
+        console.log("        "  + ops[k].name + "'s Parameters:");
+        var parameters = tools.collectElements(ops[k], "_type", "UMLParameter")
+        var numParam=0
+        for (var x in parameters){
+          numParam += 1
+          console.log("         " + parameters[x].name )
+          atts.forEach(function(att){
+            if (att.name==parameters[x].name){
+              methAttAssoc+=1
+            }
+          })
+          var parameterId= parameters[x].type.$ref;
+          classes.forEach(function(clas) {
+            if(clas._id == parameterId){
+              console.log("            " + clas.name)
+            }
+          })
+        }
       }
+      var totalPossible = numAtts*numOps;
+      console.log("total possible" + totalPossible)
+      var cohesion = methAttAssoc/totalPossible;
+      console.log(classes[i].name + "'s Cohesion: " +  cohesion);
    }
 };
 
@@ -25,5 +57,20 @@ var main = function() {
       rl.close();
     });
 };
+
+var Table = require('cli-table2');
+
+// instantiate
+var table = new Table({
+    head: ['Classes', 'Cohesion', 'Stability', 'Responsibility', 'Deviance']
+  , colWidths: [100,100,100,100,100]
+});
+
+table.push(
+    ['First value', 'Second value']
+  , ['First value', 'Second value']
+);
+
+console.log(table.toString());
 
 main();
